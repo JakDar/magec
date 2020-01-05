ecdf2d9b52591980e8e16ddf73c105d681a1a9d9
Jakub Darul <daruljakub@protonmail.com>
2020-01-05 12:02:58 +0100

    hadrcoded output dir

--- training/pl/train.sh
+++ training/pl/train.sh
@@ -5,16 +5,17 @@ MARIAN=../../models/tools/marian-dev/build
 DATA=../../data/pl/split
 SPM_DATA=../../data/pl/spm
 MODELDIR='.'
+OUT='/content/drive/My\ Drive/nlp/out'
 
 # Before vocab
 # --data-weighting "$DATA"/mono.enchant.spell.tok.w2.gz --data-weighting-type word \
 #after macx length
 # --vocabs "$DATA"/vocab.{spm,spm} --tied-embeddings-all \
 "$MARIAN"/marian --type transformer \
-	--model "$MODELDIR"/model.npz \
+	--model "$OUT"/model.npz \
 	--train-sets "$DATA"/train.err.txt.gz "$DATA"/train.cor.txt.gz --shuffle-in-ram --tempdir tmp \
 	--max-length 150 \
-  --vocabs "$SPM_DATA"/vocab.{spm,spm} --tied-embeddings-all \
+  --vocabs "$OUT"/vocab.{spm,spm} --tied-embeddings-all \
 	--enc-depth 6 --dec-depth 6 --transformer-heads 8 \
 	--dropout-src 0.2 --dropout-trg 0.1 --transformer-dropout 0.3 --transformer-dropout-ffn 0.1 --transformer-dropout-attention 0.1 \
 	--exponential-smoothing --label-smoothing 0.1 \
@@ -24,10 +25,10 @@ MODELDIR='.'
 	--cost-type cross-entropy \
 	--valid-metrics cross-entropy translation perplexity \
 	--valid-sets "$DATA"/dev.err.txt.gz "$DATA"/dev.cor.txt.gz \
-	--valid-translation-output "$MODELDIR"/devset.out --quiet-translation \
+	--valid-translation-output "$OUT"/devset.out --quiet-translation \
 	--valid-script-path "$MODELDIR"/validate.sh \
 	--valid-mini-batch 16 --beam-size 12 --normalize 1.0 \
 	--early-stopping 5 --after-epochs 5 \
 	--valid-freq 5000 --save-freq 5000 --disp-freq 500 --disp-first 5 \
 	--overwrite --keep-best \
-	--log "$MODELDIR"/train.log --valid-log "$MODELDIR"/valid.log
+	--log "$OUT"/train.log --valid-log "$OUT"/valid.log
