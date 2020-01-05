fbcf0f6be304342d7f0dfe9add8cd64bdc0de51e
Jakub Darul <daruljakub@protonmail.com>
2020-01-05 14:14:09 +0100

    changed params to working ones

--- training/pl/train.sh
+++ training/pl/train.sh
@@ -14,14 +14,14 @@ OUT=/out
 # --vocabs "$DATA"/vocab.{spm,spm} --tied-embeddings-all \
 "$MARIAN"/marian --type transformer \
 	--model "$OUT"/model.npz \
-	-w 7000 \
+	-w 6000 \
 	--train-sets "$DATA"/train.err.txt.gz "$DATA"/train.cor.txt.gz --shuffle-in-ram --tempdir tmp \
 	--max-length 150 \
 	--vocabs "$OUT"/vocab.{spm,spm} --tied-embeddings-all \
 	--enc-depth 6 --dec-depth 6 --transformer-heads 8 \
 	--dropout-src 0.2 --dropout-trg 0.1 --transformer-dropout 0.3 --transformer-dropout-ffn 0.1 --transformer-dropout-attention 0.1 \
 	--exponential-smoothing --label-smoothing 0.1 \
-	--mini-batch-fit --mini-batch 10 --maxi-batch 10 --sync-sgd --optimizer-delay 4 \
+	--mini-batch-fit --mini-batch 1000 --maxi-batch 1000 --sync-sgd --optimizer-delay 4 \
 	--learn-rate 0.0003 --lr-warmup 16000 --lr-decay-inv-sqrt 16000 --lr-report \
 	--optimizer-params 0.9 0.98 1e-09 --clip-norm 0 \
 	--cost-type cross-entropy \
