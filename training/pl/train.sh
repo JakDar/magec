1dba26b9e84583749a071d5b4d1a202c3f263e9d
Jakub Darul <daruljakub@protonmail.com>
2020-03-14 11:59:31 +0100

    sanity checks

--- training/pl/train.sh
+++ training/pl/train.sh
@@ -18,15 +18,14 @@ OUT=/out
 	# --train-sets "$DATA"/train.err.txt.gz "$DATA"/train.corr.txt.gz --shuffle-in-ram --tempdir tmp \
 "$MARIAN"/marian --type transformer \
 	--model "$OUT"/model.npz \
-	--vocabs "$OUT"/vocab.{spm,spm} --tied-embeddings-all \
-	-w 6000 \
 	--train-sets "$DATA"/train.err.txt.gz "$DATA"/train.corr.txt.gz --no-shuffle --tempdir tmp \
-	--max-length 150 \
 	--data-weighting "$DATA"/weights.txt --data-weighting-type word \
+	--vocabs "$OUT"/vocab.{spm,spm} --tied-embeddings-all \
+	--max-length 150 \
 	--enc-depth 6 --dec-depth 6 --transformer-heads 8 \
 	--dropout-src 0.2 --dropout-trg 0.1 --transformer-dropout 0.3 --transformer-dropout-ffn 0.1 --transformer-dropout-attention 0.1 \
 	--exponential-smoothing --label-smoothing 0.1 \
-	--mini-batch-fit --mini-batch 1000 --maxi-batch 1000 --sync-sgd --optimizer-delay 4 \
+	--mini-batch-fit -w 6000 --mini-batch 1000 --maxi-batch 1000 --sync-sgd --optimizer-delay 4 \
 	--learn-rate 0.0003 --lr-warmup 16000 --lr-decay-inv-sqrt 16000 --lr-report \
 	--optimizer-params 0.9 0.98 1e-09 --clip-norm 0 \
 	--cost-type cross-entropy \
