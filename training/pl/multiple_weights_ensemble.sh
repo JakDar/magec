834b2669450c528f53609e026b6aa0470dd08f74
Jakub Darul <daruljakub@protonmail.com>
2020-05-20 22:38:37 +0200

    ala

---
+++ training/pl/multiple_weights_ensemble.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ $# -lt 3 ]; then
+	print "usage: $0 <dir> <gpus> <models>"
+	exit 1
+fi
+
+# ROOT=.
+# TOOLS=$ROOT/tools
+# SCRIPTS=$ROOT/scripts
+DATA=/content/magec/data/pl/split #//spm/
+# MARIAN=$TOOLS/marian-dev/build
+# DATA=$ROOT/data
+MARIAN=../../models/tools/marian-dev/build
+OUT=/out
+
+ERROR_FILE=$1
+shift
+GPUS=$1
+shift
+
+for i in $(seq 1 9); do
+    magec=0.$i
+    lm=0.$((10 - i))
+    OPTIONS="-m /out/model.npz.best-cross-entropy.npz /out/lm.npz.best-ce-mean-words.npz --weights $magec $lm -v /out/vocab.spm /out/vocab.spm --mini-batch 32 --beam-size 6 --normalize 1.0 --max-length 120 --max-length-crop --quiet-translation"
+
+	cat $ERROR_FILE | $MARIAN/marian-decoder $OPTIONS -d $GPUS 2>$OUT/ensemble.stderr \
+		>$OUT/ensemble.corrected_0.$i.out
+
+done
