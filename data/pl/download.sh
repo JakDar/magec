d3d56cc891013476e2759589f89dc097532577c3
Jakub Darul <daruljakub@protonmail.com>
2020-06-21 08:46:48 +0200

    added weights to download

--- data/pl/download.sh
+++ data/pl/download.sh
@@ -9,8 +9,12 @@ for dataset in dev train; do
 	for corpus in err corr; do
 		file=${dataset}_${corpus}.gz
 
-		curl -X GET \
+		[ -f "split/$dataset.$corpus.txt.gz" ] || curl -X GET \
 			-o "split/$dataset.$corpus.txt.gz" \
 			"https://storage.googleapis.com/storage/v1/b/$bucket/o/$file?alt=media"
 	done
 done
+
+[ -f "split/weights.w2.gz" ] || curl -X GET \
+	-o "split/weights.w2.gz" \
+	"https://storage.googleapis.com/storage/v1/b/$bucket/o/weights.w2.gz?alt=media"
