bb9a31cca300f4679bde27ac0a061525bf9e8416
Jakub Darul <daruljakub@protonmail.com>
2020-06-23 23:38:43 +0200

    minor

--- data/pl/evaluation/m2scorer/make_gold.fish
+++ data/pl/evaluation/m2scorer/make_gold.fish
@@ -6,8 +6,8 @@ function preprocess
     sd '[0-9]' '1' | python ../../all/word_tokenize.py >$argv[2] # replace all numbers to ones, keeping length
 end
 
-test -f out/before || preprocess ../wikied_v1_before out/before
-test -f out/after || preprocess ../wikied_v1_after out/after
+test -f out/before_v1 || preprocess ../wikied_v1_before out/before_v1
+test -f out/after_v1 || preprocess ../wikied_v1_after out/after_v1
 # test -f out/after_for_marian
 
 # scripts/edit_creator.py ../wikied_v1_before data/single_after_for_marian >data/gold_for_marian
