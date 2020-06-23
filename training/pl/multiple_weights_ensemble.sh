af50fccc9a9abd405db4adb3e56789aadcd0bb91
Jakub Darul <daruljakub@protonmail.com>
2020-06-23 22:55:30 +0200

    added wikied v1 for evaluation

--- training/pl/multiple_weights_ensemble.sh
+++ training/pl/multiple_weights_ensemble.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ $# -lt 2 ]; then
-	print "usage: $0 <dir> <gpus> <models>"
-	exit 1
-fi
+# if [ $# -lt 2 ]; then
+# 	print "usage: $0 <dir> <gpus> <models>"
+# 	exit 1
+# fi
 
-# ROOT=.
-# TOOLS=$ROOT/tools
-# SCRIPTS=$ROOT/scripts
-DATA=/content/magec/data/pl/split #//spm/
+# DATA=/content/magec/data/pl/split
+# DATA=$SCRATCH/magec/data/pl/split # Prometheus
 # MARIAN=$TOOLS/marian-dev/build
 # DATA=$ROOT/data
 MARIAN=../../models/tools/marian-dev/build
-OUT=/out
+# OUT=/out
+OUT=/$SCRATCH/magecout/
 
+mkdir -p "$OUT/evaluate1nmt"
+
+#../../data/pl/evaluation/wikied_v1_before
 ERROR_FILE=$1
 shift
 GPUS=$1
@@ -23,9 +25,9 @@ shift
 for i in $(seq 1 9); do
     magec=0.$i
     lm=0.$((10 - i))
-    OPTIONS="-m /out/model.npz.best-cross-entropy.npz /out/lm.npz.best-ce-mean-words.npz --weights $magec $lm -v /out/vocab.spm /out/vocab.spm --mini-batch 32 --beam-size 6 --normalize 1.0 --max-length 120 --max-length-crop --quiet-translation"
+    OPTIONS="-m $OUT/model1/model.npz.best-cross-entropy.npz $OUT/lm1/lm.npz.best-ce-mean-words.npz --weights $magec $lm -v $OUT/vocab.spm $OUT/vocab.spm --mini-batch 32 --beam-size 6 --normalize 1.0 --max-length 120 --max-length-crop --quiet-translation"
 
-	cat $ERROR_FILE | $MARIAN/marian-decoder $OPTIONS -d $GPUS 2>$OUT/ensemble.stderr \
-		>$OUT/ensemble.corrected_0.$i.out
+	cat "$ERROR_FILE" | $MARIAN/marian-decoder $OPTIONS -d $GPUS 2>$OUT/ensemble.stderr \
+		>$OUT/evaluate1nmt/ensemble.corrected_0.$i.out
 
 done
